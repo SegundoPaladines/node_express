bcd6dfb4aff2d5269adc9412d14ff93f9aace4f3
SegundoPaladines <segundopaladines@gmail.com>
2023-12-02 08:37:44 -0500

    Ajustes del package.json

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 
+//instanciar express
 const app = express();
+
+//la constante que guarda el puerto
 const port = 3000;
 
+//el requirement y el response que permiten tomar la solicitud y la respues al server
 app.get('/', (req, res) => {
-  res.send(`<h1>SAPA PERRA</h1>`)
+  res.send(`<h1>Hola mundo</h1>`)
 })
 
+//Escuchar por el puerto port, que guarda el valor 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
